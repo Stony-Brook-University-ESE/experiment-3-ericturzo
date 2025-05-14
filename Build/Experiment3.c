50a7fd1dc0846c9877b9f60397a77146f4777465
eric turzo <ericturzo1@gmail.com>
2025-05-14 13:48:42 -0400

    update

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#define N 1000
 int main() {
-    int n = 1000;
-    int matrix[n][n];
+  
+int *A = malloc(N*N*sizeof(*A));
+if(A==NULL){perror("malloc");return (EXIT_FAILURE);}
+   
+for (int i=0;i<N*N;i++){
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = i * n + j + 1;
-        }
-    }
+A[i]=i+1;
 
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
+
+}
+
+
+for (int i=N*N-1;i>=0;i--){
+printf("%d ",A[i]);
+
+}
+printf("\n");
+free(A);
 
     return 0;
 }
